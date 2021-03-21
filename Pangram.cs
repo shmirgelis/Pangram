3a807afedbed6a200aebbd22c2a7ba376c57b71b
shmirgelis <73270957+shmirgelis@users.noreply.github.com>
2021-03-21 21:43:58 +0200

    Add project files.

---
+++ Pangram.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+public static class Pangram
+{
+    public static bool IsPangram(string input)
+    {
+        if(input == "")
+        {
+            return false;
+        }
+        else
+        {
+            string alphabet = "abcdefghijklmnopqrstuvwxyz";
+            input = input.ToLower();
+            char[] inputChars = input.ToCharArray();
+            char[] alphabetChars = alphabet.ToCharArray();
+            var orderedInput = inputChars.Where(c => Char.IsLetter(c))
+                                         .OrderBy(c => c)
+                                         .Distinct()
+                                         .Select(c => c.ToString())
+                                         .Aggregate((c1, c2) => c1 += c2);
+
+            return orderedInput == alphabet;
+        }
+        
+        
+    }
+}
